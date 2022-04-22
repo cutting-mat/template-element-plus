810d5caac7cc391db5ed99917c35c4c802e43620
tower1229 <tower1229@foxmail.com>
2022-04-22 09:42:31 +0800

    0.1.0

--- src/core/util/axiosInstance.js
+++ src/core/util/axiosInstance.js
@@ -1,14 +1,16 @@
-//import axios from 'axios';
+// import axios from 'axios';
 import axios from '@cutting-mat/axios';
 import { event, routeGenerator } from '@/core'
-import requestConfig from '@/request.config';
-console.log('[Core] Request Start.')
+import { default as requestConfig, CryptoConfig } from '@/request.config';
 import { ElMessage } from 'element-plus';
+
+console.log(`[Core] Request Start. Encryption: ${CryptoConfig.enable ? 'Enabled' : 'Disabled'}`);
+
 // 创建请求实例
 const instance = axios.create(requestConfig);
 
 // 请求前处理
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use(async function (config) {
     if (config.method === 'get' || config.method === 'delete') {
         // 过滤get和delete请求中的空字符参数
         for (let x in config.params) {
@@ -19,11 +21,26 @@ instance.interceptors.request.use(function (config) {
         }
     }
     // 如果没有开启请求控制，则删除相关自定义头，否则需要服务端设置 Access-Control-Allow-Headers
-    const permissionConfig = import.meta.glob("@/plugin.permission.config").default;
+    let permissionConfig = await import("@/plugin.permission.config.js");
+    permissionConfig = permissionConfig.default;
     if (!(permissionConfig && permissionConfig.AccessControl && permissionConfig.interceptorsRequest)) {
         delete config.headers["X-Request-Permission"];
     }
-
+    // 请求加密
+    if (CryptoConfig.enable) {
+        const SecretKey = CryptoConfig.getSecretKey(config)
+        if (SecretKey) {
+            CryptoConfig.debug && console.log('请求加密', config.url)
+            const encData = CryptoConfig.encryptData(config, SecretKey);
+            CryptoConfig.debug && console.log('encData', encData)
+            // 发送加密请求
+            config.url = CryptoConfig.getCryptoUrl(config);
+            config.method = 'post';
+            config.data = { data: encData };
+            config.params = {}
+            return config;
+        }
+    }
     return config;
 }, function (error) {
     return Promise.reject(error);
@@ -31,10 +48,17 @@ instance.interceptors.request.use(function (config) {
 
 // 响应后处理
 instance.interceptors.response.use(function (response) {
-    // 业务失败处理(兼容原response.data.code格式)
-    if (response.data.code === 500) {
-        return catchError({ response })
+    // 请求解密
+    if (CryptoConfig.enable) {
+        const SecretKey = CryptoConfig.getSecretKey(response.config)
+        if (SecretKey) {
+            CryptoConfig.debug && console.log('请求解密', response)
+            response = CryptoConfig.decryptResponse(response, SecretKey)
+            CryptoConfig.debug && console.log('response', response)
+        }
+
     }
+
     // token临近过期, 重新签发token
     if (response.headers['jwt-update-token']) {
         event.emit('login', {
@@ -44,12 +68,16 @@ instance.interceptors.response.use(function (response) {
             }
         })
     }
+
     // 控制台输出接口返回
     if (window.DebugRequest) {
         console.log(response.request.custom.options.type, response.request.custom.options.url, ' => ', response.data)
     }
 
-    // 手动剥离一层data
+    // (兼容response.data.code格式)
+    if (response.data.code === 500) {
+        return catchError({ response })
+    }
     if (response.data.data) {
         response.data = response.data.data
     }
--- src/request.config.js
+++ src/request.config.js
@@ -1,7 +1,8 @@
+import CryptoJS from "crypto-js";
 
 // 各环境BaseURL配置
 const ENVIRONMENT = {
-    mock: 'http://yapi.sogdata.com/mock/125',   // 内网YAPI项目
+    mock: 'http://yapi.sogdata.com/mock/125', // 内网YAPI项目
     dev: 'http://retire.kaifa/japi',
     test: 'http://test.com/api',
     master: '//master.com/api'
@@ -20,5 +21,89 @@ export default {
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
+    }
+}
+/**
+ * AES加密方法
+ * @param content [String]: 明文
+ * @param secretKey [String]: 密钥
+ * @return encDataStr [String]: 密文
+ * */
+const enCrypto = (content, secretKey) => {
+    if (secretKey) {
+        const salt = CryptoJS.lib.WordArray.random(8)
+        const iv = CryptoJS.lib.WordArray.create(CryptoJS.SHA256(salt).words.slice(0, 16))
+        const key = CryptoJS.PBKDF2(secretKey, salt, { iterations: 1, keySize: 128 / 32 })
+        const encryptData = CryptoJS.AES.encrypt(content, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        })
+        const encDataStr = CryptoJS.lib.WordArray.create([0x53616c74, 0x65645f5f]).concat(salt).concat(encryptData.ciphertext).toString(CryptoJS.enc.Base64)
+        return encDataStr
+    } else {
+        console.warn('enCrypto(): 缺少 secretKey')
+        return null;
+    }
+}
+/**
+ * AES解密方法
+ * @param content [String]: 密文
+ * @param secretKey [String]: 密钥
+ * @return result [String]: 明文
+*/
+const deCrypto = (content, secretKey) => {
+    if (secretKey) {
+        const b64 = CryptoJS.enc.Base64.parse(content);
+        const salt = CryptoJS.lib.WordArray.create(b64.words.slice(2, 4))
+        const iv = CryptoJS.lib.WordArray.create(CryptoJS.SHA256(salt).words.slice(0, 16))
+        const key = CryptoJS.PBKDF2(secretKey, salt, { iterations: 1, keySize: 128 / 32 })
+        const eContent = CryptoJS.lib.WordArray.create(b64.words.slice(4)).toString(CryptoJS.enc.Base64)
+        const result = CryptoJS.AES.decrypt(eContent, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        }).toString(CryptoJS.enc.Utf8)
+        return result
+    } else {
+        console.warn('deCrypto(): 缺少 secretKey')
+        return null;
+    }
+}
+// 请求加密相关配置
+export const CryptoConfig = {
+    enable: false, // process.env.NODE_ENV === 'production', // 加密开关
+    debug: process.env.NODE_ENV === 'development', // 调试开关
+    getCryptoUrl: (requestConfig) => { // 自定义加密请求地址方法
+        return `/sapi/${CryptoJS.MD5(requestConfig.url + '|sogdata.com')}`
+    },
+    getSecretKey: requestConfig => { // 自定义加解密密钥方法
+        const token = requestConfig.headers.Authorization || (requestConfig.headers.common && requestConfig.headers.common.Authorization);
+        if (token) {
+            console.log(token.split('.'))
+            return token.split('.')[2];
+        } else {
+            return null
+        }
+    },
+    encryptData: function (requestConfig, SecretKey) { // 自定义加密方法
+        const baseParam = {
+            path: `${requestConfig.url}?_ts=${new Date().getTime()}`,
+            method: requestConfig.method,
+            params: (requestConfig.data || requestConfig.params || {}),
+            device: 'admin'
+        }
+        return enCrypto(JSON.stringify(baseParam), SecretKey);
+    },
+    decryptResponse: function (response, SecretKey) { // 自定义解密方法
+        if (SecretKey && response.data.split) {
+            const decData = deCrypto(response.data, SecretKey)
+            if (decData) {
+                response.data = JSON.parse(decData)
+            } else {
+                console.warn(`decryptResponse(): 解密失败`)
+            }
+        }
+        return response
     }
 }
