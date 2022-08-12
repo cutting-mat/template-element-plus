9f2f1ca3da52e1d9dece57896dc2bd97a0b5a2c0
tower1229 <tower1229@foxmail.com>
2022-08-12 13:43:30 +0800

    更新依赖

--- src/core/components/index.js
+++ src/core/components/index.js
@@ -1,9 +1,12 @@
 // 搜索vue组件（排除以'__'开头）
-const ctx = import.meta.globEager("./!(__)*.vue");
+const ctx = import.meta.glob("./!(__)*.vue", { eager: true });
 const components = {};
 Object.keys(ctx).forEach((key) => {
-    const fileName = key.replace(/^\.\//, "").replace(/\.vue$/, "").replace(/\//g, "-");
-    components[fileName] = ctx[key].default
-})
+  const fileName = key
+    .replace(/^\.\//, "")
+    .replace(/\.vue$/, "")
+    .replace(/\//g, "-");
+  components[fileName] = ctx[key].default;
+});
 // console.log('core components:', components)
 export default components;
