e44f96c5af144721e5f998dd1d19b62d4f17af0d
tower1229 <tower1229@foxmail.com>
2022-08-14 19:43:42 +0800

    更新依赖版本

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'unplugin-element-plus/vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import ElementPlus from "unplugin-element-plus/vite";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    ElementPlus()
-  ],
+  plugins: [vue(), ElementPlus()],
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/template-element-plus/'     // 生产环境构建路径
-    : '/',
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/template-element-plus/" // 生产环境构建路径
+      : "/",
   build: {
-    outDir: 'dist'
-  }
-})
+    minify: "terser",
+    manifest: false,
+    sourcemap: false,
+    outDir: "dist",
+  },
+});
